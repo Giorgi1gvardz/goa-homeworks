23a54077d869f2bc031d088814f94ad11d054f49
Giorgi1gvardz <SANDROSIA1991@mail.ru>
2024-02-27 21:46:08 +0300

    commit

---
+++ day06/homework/day06.py
@@ -0,0 +1,11 @@
+user_name = input("what is your name ")
+
+last_name = input("what is your ")
+
+user_home = input("where do you live ")
+
+date_of_birth = input("what year were you born ")
+
+user_hige = input("what year were you born ")
+
+print("your name is" user_name +"your last nameis"+ last_name +""+ user_home +""+ date_of-birth +""+ user_high ) 
